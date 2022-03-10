d804ac4bf658c965eb77832fdf8014cfacc3b7a7
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-03-09 21:31:32 -0500

    Se instala PrimeFlex y se usan los pipes uppercase, lowercase y titlecase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRouterModule,
     SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
