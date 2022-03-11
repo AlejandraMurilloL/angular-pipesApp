cc8af803ef8b7aa7df383c51af2281655149a143
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-03-10 19:30:57 -0500

    i18nSelect Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  nombre: string = 'Alejandra mUrilLo';
-  valor: number = 1000;
-  persona: any = {
-    nombre: 'Alejandra'
-  };
+  constructor(private primeNgConfig: PrimeNGConfig) { }
 
-  mostrarValores(): void {
-    console.log(this.nombre);
-    console.log(this.valor);
-    console.log(this.persona);
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import localeEs from "@angular/common/locales/es-CO";
 import localeFr from "@angular/common/locales/fr";
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRouterModule } from './app-router.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -21,7 +22,8 @@ registerLocaleData(localeFr);
     BrowserModule,
     AppRouterModule,
     SharedModule,
-    VentasModule
+    VentasModule,
+    BrowserAnimationsModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'es-CO' }
