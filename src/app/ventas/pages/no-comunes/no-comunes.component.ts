ea1f3443af148e606bd84f90a12b99b2a1a88fe3
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-04 15:12:14 -0500

    Se anyade async Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -54,5 +55,15 @@ export class NoComunesComponent {
       nombre: 'Aquaman',
       vuela: false
     }
-  ]
+  ];
+
+  // Async Pipe
+  miObservable = interval(1000); // 0,1,2,3,4,5,6,7...
+
+  valorPromesa =  new Promise((resolve, rejec) => {
+    setTimeout(() => {
+      resolve("Tenemos data de promesa");
+    }, 3500);
+  });
+
 }
