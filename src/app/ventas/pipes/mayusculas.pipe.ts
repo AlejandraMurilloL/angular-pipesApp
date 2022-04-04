576a11737d9188892035c648c7a2af70692b05ed
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-04 15:54:00 -0500

    Se crea pipe personalizado "Mayusculas"

---
+++ src/app/ventas/pipes/mayusculas.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    'name' : 'mayusculas'
+})
+export class MayusculasPipe implements PipeTransform {
+
+    transform(value: string, esMayuscula: boolean = true): string {
+        return esMayuscula ? value.toUpperCase() : value.toLowerCase();
+    }
+}
